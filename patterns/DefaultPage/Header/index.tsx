173474cdd0e001f7833e289ad4c2102ef29bc52b
Guilherme Almeida <iugmali@gmail.com>
2022-04-05 17:29:33 -0300

    cinema-next first files

---
+++ patterns/DefaultPage/Header/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import styles from './DefaultPage.module.scss';
+
+type Props = {
+
+};
+const Header = (props: Props) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+export default Header;
---
+++ patterns/DefaultPage/index.tsx
@@ -0,0 +1,15 @@
+// @flow
+import * as React from 'react';
+import styles from './DefaultPage.module.scss';
+
+type Props = {
+
+};
+const DefaultPage = (props: Props) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+export default DefaultPage;
