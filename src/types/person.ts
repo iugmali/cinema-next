8e087bdbb39b944d84fff5a8c1c6a9d700252f2d
Guilherme Almeida <iugmali@gmail.com>
2022-04-08 01:56:47 -0300

    ajustes em types

--- src/types/person.ts
+++ src/types/person.ts
@@ -1,9 +1,9 @@
-import {Media} from "./media";
+import {MediaItem} from "./media";
 
 export interface Person {
   id: string;
   profile_path: string;
   name: string;
   known_for_department: string;
-  known_for: Media[]
+  known_for: MediaItem[];
 }
